14edfd69a9dde0ae86ce8ccb09de19860085721b
github-ci <you@example.com>
2025-06-13 14:57:41 +0800

    新增对 `tourze/bundle-dependency` 和 `tourze/doctrine-timestamp-bundle` 的依赖，完善 `WechatOfficialAccountBundle` 的依赖声明。

--- src/WechatOfficialAccountBundle.php
+++ src/WechatOfficialAccountBundle.php
@@ -2,8 +2,18 @@
 
 namespace WechatOfficialAccountBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
 
-class WechatOfficialAccountBundle extends Bundle
+class WechatOfficialAccountBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            DoctrineBundle::class => ['all' => true],
+            DoctrineTimestampBundle::class => ['all' => true],
+        ];
+    }
 }
