558076c96ebe16e08f597a251c9fbaf48f4df981
github-ci <you@example.com>
2025-11-03 17:49:04 +0800

    update

--- src/DataFixtures/AccountFixtures.php
+++ src/DataFixtures/AccountFixtures.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace WechatOfficialAccountBundle\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
--- src/Repository/CallbackIPRepository.php
+++ src/Repository/CallbackIPRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace WechatOfficialAccountBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
